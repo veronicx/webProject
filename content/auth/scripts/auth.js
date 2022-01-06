77f3a7d1d1dc5e2595dbc2a119ca854ba587e0bd
Enis Halilaj <93098680+lucy90210@users.noreply.github.com>
2022-01-06 03:02:17 +0100

    showpassword fix

--- content/auth/scripts/auth.js
+++ content/auth/scripts/auth.js
@@ -15,3 +15,12 @@ function validateLogin() {
         window.alert('wrong')
     }
 }
+
+function showPassword() {
+    var pw = document.getElementById("password");
+    if (pw.type === "password") {
+        pw.type = "text";
+    } else {
+        pw.type = "password";
+    }
+}
